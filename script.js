e09d678c828d0006abee8ebf85ccacd5337354e1
aditidtdt <aditidtdt@gmail.com>
2025-06-25 14:59:50 +0530

    built grid basis

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let color = '#';
 function randomColor(){
   let letters = "0123456789ABCDEF";
+  let color = '#';
   for (let i = 0; i < 6; i++) {
     color += letters[(Math.floor(Math.random() * 16))];
   }
