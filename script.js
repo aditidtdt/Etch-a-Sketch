430c58e366e06d683e8e0b15690cd821d8183d77
aditidtdt <aditidtdt@gmail.com>
2025-06-25 14:47:19 +0530

    built the grid and user input for grid size

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+let color = '#';
+function randomColor(){
+  let letters = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    color += letters[(Math.floor(Math.random() * 16))];
+  }
+  return color;
+}
+
+let cell;
+function createGrid(size) {
+  const grid = document.querySelector('#grid'); 
+  grid.innerHTML = '';
+  const cellSize = 600 / size;
+
+  for (let i = 1; i <= size*size; i++){
+    cell = document.createElement('div');
+    cell.classList.add("cell");
+    grid.appendChild(cell);
+    cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px`;
+  }
+}
+
+createGrid(16);
+
 let numOfSquares;
 function squares(){
   numOfSquares = document.querySelector("#number").value;
+  console.log(numOfSquares);
+  createGrid(numOfSquares);
 }
 
-const grid = document.querySelector('#grid'); 
-for (let i = 1; i <= numOfSquares; i++){
-  const cell = document.createElement('div');
-  cell.className = 'grid-cell';
-  grid.appendChild(cell);
+function clear() {
+  grid.innerHTML = "";
 }
